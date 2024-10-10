86ed467f22e641bca2547e9b5cc3a101d5e8818c
Yang.he <yang.he@homepartners.tech>
2024-10-10 11:33:47 +0800

    feat: add dispatch workflow

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,39 @@ export const App = () => {
   )
 }
 const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/></section>
+const 二儿子 = () => <section>二儿子<Wrapper/></section>
 const 幺儿子 = () => <section>幺儿子</section>
 const User = () => {
   const contextValue = useContext(appContext)
   return <div>User:{contextValue.appState.user.name}</div>
 
 }
-const reducer = (state, {type, payload}) => {
-  if (type === 'updateUser') {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...payload
-      }
-    }
-  } else {
-    return false
-  }
-}
-const UserModifier = () => {
+const Wrapper = () => {
   const {appState, setAppState} = useContext(appContext)
+  const dispatch = (action) => {
+    setAppState(reducer(appState, action))
+  }
+  const reducer = (state, {type, payload}) => {
+    if (type === 'updateUser') {
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...payload
+        }
+      }
+    } else {
+      return false
+    }
+  }
+  return <UserModifier dispatch={dispatch} state={appState} />
+}
+const UserModifier = ({dispatch, state}) => {
   const onChange = (e) => {
-    setAppState(reducer(appState, {type:'updateUser', payload: {name: e.target.value}}))
+    dispatch({type:'updateUser', payload: {name: e.target.value}})
   }
   return <div>
-    <input value={appState.user.name}
+    <input value={state.user.name}
       onChange={onChange}/>
   </div>
 }
