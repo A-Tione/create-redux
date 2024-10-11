b09df35972d65304a1286cd9489ce2f2f9b878eb
Yang.he <yang.he@homepartners.tech>
2024-10-11 15:05:02 +0800

    feat: accurate render

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,20 @@ export const App = () => {
     </appContext.Provider>
   )
 }
-const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/>二儿子的崽</section>
-const 幺儿子 = () => <section>幺儿子</section>
+const 大儿子 = () => {
+  console.log('大儿子执行了 ' + Math.random())
+  return <section>大儿子<User/></section>
+}
+const 二儿子 = () => {
+  console.log('二儿子执行了 ' + Math.random())
+  return <section>二儿子<UserModifier/>二儿子的崽</section>
+}
+const 幺儿子 = connect(state => {
+  return {group: state.group}
+})(({group}) => {
+  console.log('幺儿子执行了 ' + Math.random())
+  return <section>幺儿子<div>Group: {group.name}</div></section>
+})
 const User = connect(state => {
   return {user: state.user}
 })(({user}) => {
--- src/redux.jsx
+++ src/redux.jsx
@@ -2,7 +2,8 @@ import React, {useState, useContext, useEffect} from 'react'
 
 export const store = {
   state: {
-    user: {name: 'a-tione', age: 18}
+    user: {name: 'a-tione', age: 18},
+    group: {name: '前端'}
   },
   setState(newState) {
     store.state = newState
@@ -32,16 +33,27 @@ const reducer = (state, {type, payload}) => {
   }
 }
 
+const changed = (oldState, newState) => {
+  let changed = false
+  for(let key in oldState) {
+    if(oldState[key] !== newState[key]) {
+      changed = true
+    }
+    return changed;
+  }
+}
+
 export const connect = (selector) => (Component) => {
   return (props) => {
     const {state, setState} = useContext(appContext)
     const [, update] = useState({})
     const data = selector ? selector(state) : {state}
-    useEffect(() => {
-      store.subCancel(() => {
-        update({})
-      })
-    }, [])
+    useEffect(() => store.subCancel(() => {
+      const newData = selector ? selector(store.state) : store.state
+      if(changed(data, newData)) {
+          update({})
+      }
+    }), [selector])
     const dispatch = (action) => {
       setState(reducer(state, action))
     }
