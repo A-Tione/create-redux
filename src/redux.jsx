aeba68e1115187a25a2bd6dfe05e2d027f52f8ad
Yang.he <yang.he@homepartners.tech>
2024-10-11 15:19:20 +0800

    feat: add mapDispatchToProps

--- src/redux.jsx
+++ src/redux.jsx
@@ -43,21 +43,22 @@ const changed = (oldState, newState) => {
   }
 }
 
-export const connect = (selector) => (Component) => {
+export const connect = (selector, dispatchSelector) => (Component) => {
   return (props) => {
+    const dispatch = (action) => {
+      setState(reducer(state, action))
+    }
     const {state, setState} = useContext(appContext)
     const [, update] = useState({})
     const data = selector ? selector(state) : {state}
+    const dispatchers = dispatchSelector ? dispatchSelector(dispatch) : {dispatch}
     useEffect(() => store.subCancel(() => {
       const newData = selector ? selector(store.state) : store.state
       if(changed(data, newData)) {
           update({})
       }
     }), [selector])
-    const dispatch = (action) => {
-      setState(reducer(state, action))
-    }
-    return <Component {...props} dispatch={dispatch} {...data} />
+    return <Component {...props} {...dispatchers} {...data} />
   }
 }
 
